7411f38bd9d8731d86b5b1beee5f3b202549a375
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-01-18 22:44:26 -0800

    updated the cube logo in About page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,11 +3,11 @@ import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
-  faAngular,
   faCss3,
   faGitAlt,
   faHtml5,
   faJsSquare,
+  faNode,
   faReact,
 } from '@fortawesome/free-brands-svg-icons'
 import Loader from 'react-loaders'
@@ -51,22 +51,22 @@ const About = () => {
         <div className="stage-cube-cont">
           <div className="cubespinner">
             <div className="face1">
-              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+              <FontAwesomeIcon icon={faNode} color="#3C873A" />
             </div>
             <div className="face2">
-              <FontAwesomeIcon icon={faHtml5} color="#DD0031" />
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
             </div>
             <div className="face3">
-              <FontAwesomeIcon icon={faCss3} color="#DD0031" />
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
             </div>
             <div className="face4">
-              <FontAwesomeIcon icon={faReact} color="#DD0031" />
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
             </div>
             <div className="face5">
-              <FontAwesomeIcon icon={faJsSquare} color="#DD0031" />
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
             </div>
             <div className="face6">
-              <FontAwesomeIcon icon={faGitAlt} color="#DD0031" />
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
             </div>
           </div>
         </div>
