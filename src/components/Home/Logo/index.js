469b163ce5f666e50c61365139adf485aaea0e54
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-02-14 18:24:09 -0800

    fixed gsap

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react'
-import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+import gsap from 'gsap'
+import DrawSVGPlugin from 'gsap/DrawSVGPlugin'
 import LogoS from '../../../assets/images/iqbal-photo.png'
 import './index.scss'
 
